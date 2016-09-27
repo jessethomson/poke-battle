d31a702365308eaef19b6bdb28079052df224c97
Jesse Thomson <jessethomson11@gmail.com>
2016-09-26 22:50:34 -0600

    Finished

--- js/app.js
+++ js/app.js
@@ -5,18 +5,28 @@ app.controller('pokemonCtrl', function($scope, $http, $window) {
 	var totalAvailablePokemon = 248;
 	var totalFightingPokemon = 6;
 
-	function startGame() {
-		$scope.gameHeight = window.innerHeight;
+	function resetGame() {
+		$scope.currentPokemon = null;
+		$scope.enemyPokemon = null;
 		$scope.pokemonDefeated = 0;
+		$scope.experiencedGained = 0;
 		$scope.pokemonRemaining = totalFightingPokemon;
 		$scope.playersTurn = true;
 		$scope.pokemons = [];
+	}
+
+	$scope.startGame = function() {
+
+		$( document ).ready(function() {
+			stopAudio("battle-music");
+			playAudio("battle-music");
+		});
+		resetGame();
 		loadEnemy();
 		loadPokemons();
-
-		var globalThing = {};
+		$scope.gameReady = true;
 	}
-	startGame();
+	//startGame();
 
 	function loadEnemy() {
 		var id = Math.floor((Math.random() * totalAvailablePokemon) + 1);
@@ -30,6 +40,7 @@ app.controller('pokemonCtrl', function($scope, $http, $window) {
 				name: pokemon.name.split("-")[0],
 				imageFront: pokemon.sprites.front_default,
 				imageBack: pokemon.sprites.back_default,
+				baseExperience: pokemon.base_experience,
 				totalHp: hp,
 				currentHp: hp,
 				moves: moves
@@ -57,6 +68,7 @@ app.controller('pokemonCtrl', function($scope, $http, $window) {
 					name: pokemon.name.split("-")[0],
 					imageFront: pokemon.sprites.front_default,
 					imageBack: pokemon.sprites.back_default,
+					baseExperience: pokemon.base_experience,
 					totalHp: hp,
 					currentHp: hp,
 					moves: moves
@@ -77,11 +89,10 @@ app.controller('pokemonCtrl', function($scope, $http, $window) {
 
 
 	function getMoves(pokemon) {
-		console.log(pokemon.moves);
+
 		var moves = [];
 		while(moves.length < 4) {
 			var index = Math.floor((Math.random() * (pokemon.moves.length-1)));
-			console.log(index);
 			moves.push(pokemon.moves[index].move);
 		}
 		return moves;
@@ -89,24 +100,30 @@ app.controller('pokemonCtrl', function($scope, $http, $window) {
 
 
 	$scope.attackEnemy = function(move) {
-		if($scope.playersTurn && !$scope.currentPokemon.dead) {
+		if(!$scope.currentPokemon.dead) {
+			playAudio("beep");
+			$(".current-pokemon").animate({left: '250px', bottom:'80px'});
+			$('.enemy-pokemon').ClassyWiggle();
 			$scope.playersTurn = false;
-			$scope.gameStatus = $scope.currentPokemon.name + " uses " + move.name;
+			$scope.gameStatus = capitalize($scope.currentPokemon.name) + " uses " + capitalize(move.name);
 			console.log($scope.gameStatus);
 			var url = move.url;
 			$http.get(url).then(function(response) {
-				console.log(response);
-				var newHp = $scope.enemyPokemon.currentHp - response.data.power/2;
+				$(".current-pokemon").animate({left: '0px', bottom:'0px'});
+				$('.enemy-pokemon').ClassyWiggle("stop");
+				var newHp = $scope.enemyPokemon.currentHp - (response.data.power/2 + 5);
 				if(newHp > 0) {
 					$scope.enemyPokemon.currentHp = newHp;
-					console.log("Attacking")
 					$window.setTimeout(function() {
 						$scope.attackCurrentPokemon();
 					}, 500);
 				}
 				else {
+					playAudio("death");
 					$scope.enemyPokemon.currentHp = 0;
+					$scope.enemyPokemon.dead = true;
 					$scope.pokemonDefeated++;
+					$scope.experiencedGained += $scope.enemyPokemon.baseExperience;
 					loadEnemy();
 				}
 			}, function(error) {
@@ -117,23 +134,25 @@ app.controller('pokemonCtrl', function($scope, $http, $window) {
 
 	$scope.attackCurrentPokemon = function() {
 		if(!$scope.playersTurn) {
+			$('.current-pokemon').ClassyWiggle();
 			var index = Math.floor(Math.random() * $scope.enemyPokemon.moves.length);
 			var move = $scope.enemyPokemon.moves[index];
-			$scope.gameStatus = $scope.enemyPokemon.name + " uses " + move.name;
+			$scope.gameStatus = capitalize($scope.enemyPokemon.name) + " uses " + capitalize(move.name);
 			console.log($scope.gameStatus);
 			var url = move.url;
 			$http.get(url).then(function(response) {
-
-				var newHp = $scope.currentPokemon.currentHp - response.data.power/2;
+				$('.current-pokemon').ClassyWiggle("stop");
+				var newHp = $scope.currentPokemon.currentHp - (response.data.power/2 + 5);
 				if(newHp > 0) {
 					$scope.currentPokemon.currentHp = newHp;
 				}
 				else {
+					playAudio("death");
 					$scope.currentPokemon.currentHp = 0;
 					$scope.currentPokemon.dead = true;
 					$scope.pokemonRemaining--;
 					if($scope.pokemonRemaining <= 0) {
-						$scope.gameOver = true;
+						$scope.gameReady = false;
 					}
 				}
 				$scope.playersTurn = true;
@@ -149,12 +168,26 @@ app.controller('pokemonCtrl', function($scope, $http, $window) {
 		}
 	}
 
+	function playAudio(audioId) {
+		document.getElementById(audioId).play();
+	}
+
+	function stopAudio(audioId) {
+		var sound = document.getElementById(audioId);
+		sound.pause();
+		sound.currentTime = 0;
+	}
+
 	function getPokemon(number) {
 		return $http.get("http://pokeapi.co/api/v2/pokemon/" + number);
 	}
 
 	function getPokemonForm(number) {
 		return $http.get("http://pokeapi.co/api/v2/pokemon-form/" + number);
+	}
+
+	function capitalize(input) {
+		return input.charAt(0).toUpperCase() + input.substr(1).toLowerCase();
 	}
 
 });
